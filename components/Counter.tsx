2cd41c8e962062329fd0ff4d11b05be63a439f80
konishi333 <jobhuntingkonishi3@gmail.com>
2025-01-14 17:53:25 +0900

    初期ファイルコミット

---
+++ components/Counter.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.resetButton} onPress={reset}>
+          <Text style={styles.buttonText}>リセット</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.minusButton} onPress={decrement}>
+          <Text style={styles.buttonText}>マイナス</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.countText}>{count}</Text>
+      <TouchableOpacity style={styles.countButton} onPress={increment}>
+        <Text style={styles.buttonText}>カウント</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const { height, width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+  },
+  buttonContainer: {
+    position: "absolute",
+    top: height * 0.01,
+    height: height * 0.15,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  resetButton: {
+    width: width * 0.46,
+    backgroundColor: "#FF6347",
+    padding: 5,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  minusButton: {
+    width: width * 0.46,
+    marginLeft: width * 0.02,
+    backgroundColor: "#FF8C00",
+    padding: 5,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  countButton: {
+    position: "absolute",
+    bottom: 8,
+    height: height * 0.3,
+    width: width * 0.96,
+    backgroundColor: "#4660B4",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    padding: 5,
+    color: "white",
+    fontSize: 30,
+  },
+  countText: {
+    position: "absolute",
+    top: height * 0.2,
+    fontSize: 150,
+  },
+});
